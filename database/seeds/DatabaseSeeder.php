7a7c019f0ad785ee9247489091511cc47e880945
Sasha Miljkovic <imalimalomrk@gmail.com>
2018-04-25 20:54:21 +0200

    Countries added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserRolesSeeder::class);
         $this->call(SuperAdminSeed::class);
+
+        $this->call('CountriesSeeder');
+        $this->command->info('Seeded the countries!'); 
     }
 }
