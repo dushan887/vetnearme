004d7a46fe4b914902fa43fe58af540d62355541
Sasha Miljkovic <imalimalomrk@gmail.com>
2018-04-25 20:36:21 +0200

    clinics added

--- app/Clinic.php
+++ app/Clinic.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Clinic extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description', 'email', 'phone_number', 'phone_number2', 'address', 'lat', 'lng', 'gmaps_link',
+        'social_media', 'opening_hours', 'logo', 'owner_id'
+    ];
+
     public function services()
     {
        return $this->belongsToMany(Services::class, 'clinics_services');
