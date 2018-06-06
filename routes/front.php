076a8ab6a1af4248d7f2c994e77e0f02facd8a15
top-dev <dushan887@gmail.com>
2018-06-06 20:21:25 +0200

    Single Clinic

--- routes/front.php
+++ routes/front.php
@@ -2,4 +2,4 @@
 
     Route::get('/', 'Front\FrontPartController@index')->name('home');
     Route::get('/results', 'Front\ResultsController@index')->name('results');
-    Route::get('/clinic', 'Front\ClinicPartController@index')->name('clinic');
+    Route::get('/clinic', 'Front\ClinicController@index')->name('clinic');
