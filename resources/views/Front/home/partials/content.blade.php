a14cd953f91c7de4235985438814ef2bd37811c5
top-dev <dushan887@gmail.com>
2018-06-19 01:06:31 +0200

    blog

--- resources/views/Front/home/partials/content.blade.php
+++ resources/views/Front/home/partials/content.blade.php
@@ -1,20 +1,20 @@
 <section class="blog-grid grid-2x2">
 	<div class="row">
-		<div class="col-12 col-md-6">
+	<div class="col-12 col-md-4">
 			<article>
 				<header>
 					<a href="#">
-						<div class="cover-img" style="background-image: url(img/banner.jpg);"></div>
+						<div class="cover-img" style="background-image: url({{ asset('/img/post2.jpg') }});"></div>
 					</a>
 				</header>
 				<div class="article-body">
-					<h3><a href="#">What is Lorem Ipsum?</a></h3>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+					<h3><a href="#">Fireworks - top 10 tips!</a></h3>
+					<p>We are lucky enough in Canberra to enjoy fireworks at many events! But the fun and frivolity is often lost on our pets! Some of them...</p>
 				</div>
 				<footer>
 					<div class="row">
 						<div class="col-12 col-md-6 align-left">
-							<a href="#">CATGEGORY</a>
+							<a href="#">PET HEALTH</a>
 						</div>
 						<div class="col-12 col-md-6 align-right">
 							<a href="#">read more...</a>
@@ -24,21 +24,21 @@
 			</article>
 		</div>
 		
-		<div class="col-12 col-md-6">
+		<div class="col-12 col-md-4">
 			<article>
 				<header>
 					<a href="#">
-						<div class="cover-img" style="background-image: url(img/banner4.jpg);"></div>
+						<div class="cover-img" style="background-image: url({{ asset('/img/post3.jpg') }});"></div>
 					</a>
 				</header>
 				<div class="article-body">
-					<h3><a href="#">What is Lorem Ipsum?</a></h3>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+					<h3><a href="#">Ticks - the quiet killers!</a></h3>
+					<p>We are again warning pet owners to be vigilant after an unprecedented number of tick paralysis cases in Queensland, NSW and ACT for...</p>
 				</div>
 				<footer>
 					<div class="row">
 						<div class="col-12 col-md-6 align-left">
-							<a href="#">CATGEGORY</a>
+							<a href="#">MEDICAL BULLETIN</a>
 						</div>
 						<div class="col-12 col-md-6 align-right">
 							<a href="#">read more...</a>
@@ -47,24 +47,22 @@
 				</footer>
 			</article>
 		</div>
-	</div>
 
-	<div class="row">
-		<div class="col-12 col-md-6">
+		<div class="col-12 col-md-4">
 			<article>
 				<header>
 					<a href="#">
-						<div class="cover-img" style="background-image: url(img/banner3.jpg);"></div>
+						<div class="cover-img" style="background-image: url({{ asset('/img/post4.jpg') }});"></div>
 					</a>
 				</header>
 				<div class="article-body">
-					<h3><a href="#">What is Lorem Ipsum?</a></h3>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+					<h3><a href="#">Beware of snakes</a></h3>
+					<p>With the arrival of spring, snakes are posing a greater threat to pets as the warmer weather and dryer conditions put an end to their...</p>
 				</div>
 				<footer>
 					<div class="row">
 						<div class="col-12 col-md-6 align-left">
-							<a href="#">CATGEGORY</a>
+							<a href="#">VETERINARY</a>
 						</div>
 						<div class="col-12 col-md-6 align-right">
 							<a href="#">read more...</a>
@@ -73,21 +71,22 @@
 				</footer>
 			</article>
 		</div>
-		<div class="col-12 col-md-6">
+
+		<div class="col-12 col-md-4">
 			<article>
 				<header>
 					<a href="#">
-						<div class="cover-img" style="background-image: url(img/banner5.jpg);"></div>
+						<div class="cover-img" style="background-image: url({{ asset('/img/post5.jpg') }});"></div>
 					</a>
 				</header>
 				<div class="article-body">
-					<h3><a href="#">What is Lorem Ipsum?</a></h3>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+					<h3><a href="#">Tooth brushing in dogs and cats</a></h3>
+					<p>Pets can’t brush their teeth, but just like people, dogs and cats are at risk for dental problems that can cause pain and serious...</p>
 				</div>
 				<footer>
 					<div class="row">
 						<div class="col-12 col-md-6 align-left">
-							<a href="#">CATGEGORY</a>
+							<a href="#">MEDICAL BULLETIN</a>
 						</div>
 						<div class="col-12 col-md-6 align-right">
 							<a href="#">read more...</a>
