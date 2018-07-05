74f3bdff2551cc8cd80e547e1187eca4e62d5764
top-dev <dushan887@gmail.com>
2018-07-05 18:09:48 +0200

    Blog grid/view homepage

--- resources/views/Front/home/partials/content.blade.php
+++ resources/views/Front/home/partials/content.blade.php
@@ -1,9 +1,9 @@
-<section class="blog-grid grid-2x2">
+<section class="blog-grid grid-3x3 home-blog">
 	<div class="row">
 
 		@foreach($posts as $post)
 
-			<div class="col-12 col-md-6">
+			<div class="col-12 col-md-4">
 				<article>
 
 					<header>
@@ -17,10 +17,10 @@
 						<h3>
 							<a href="/blog/{{ $post->permalink }}">{{ $post->title }}!</a>
 						</h3>
-						<p>{{ $post->expert }}</p>
+						{{-- <p>{{ $post->expert }}</p> --}}
 					</div>
 
-					<footer>
+					{{-- <footer>
 						<div class="row">
 							<div class="col-12 col-md-6 align-left">
 								<a href="#">PET HEALTH</a>
@@ -28,12 +28,13 @@
 							<div class="col-12 col-md-6 align-right">
 								<a href="/blog/{{ $post->permalink }}">read more...</a>
 							</div>
-						</div>
+						</div> --}}
 					</footer>
 				</article>
 			</div>
 
 		@endforeach
+	</div>
 
 		<div class="row">
 			<div class="col-12" align="center">
