d8281c5a51cb2684b937be7c359af5202eae88b6
Sasha Miljkovic <imalimalomrk@gmail.com>
2018-07-10 14:27:47 +0200

    Added 404 page for front

---
+++ resources/views/Front/404.blade.php
@@ -0,0 +1,12 @@
+@extends('Front.main.main-template')
+
+@section('SEOinfo')
+	<title>Vet Near Me | 404</title>
+@stop
+
+@section('BodySetup')
+
+    <h1>404</h1>
+    <h3>This page doesn't exist</h3>
+
+@endsection
