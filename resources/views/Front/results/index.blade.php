aa7d170a501ac34eb64d28b3bbcaa82fd1a3aac8
top-dev <dushan887@gmail.com>
2018-09-21 00:01:57 +0200

    restore ne umem

--- resources/views/Front/results/index.blade.php
+++ resources/views/Front/results/index.blade.php
@@ -338,48 +338,10 @@ function initMap() {
 	 };
 	let input        = document.getElementById('address-input');
 	let autocomplete = new google.maps.places.Autocomplete(input,options);
-	autocomplete.setComponentRestrictions(
-            {'country': ['AU']});
   map = new google.maps.Map(document.getElementById('map'), {
     center: pyrmont,
     zoom: 15
   });
-
-  autocomplete.addListener('place_changed', function() {
-          infowindow.close();
-          marker.setVisible(false);
-          var place = autocomplete.getPlace();
-          if (!place.geometry) {
-            // User entered the name of a Place that was not suggested and
-            // pressed the Enter key, or the Place Details request failed.
-            window.alert("No details available for input: '" + place.name + "'");
-            return;
-          }
-
-          // If the place has a geometry, then present it on a map.
-          if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-          } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);  // Why 17? Because it looks good.
-          }
-          marker.setPosition(place.geometry.location);
-          marker.setVisible(true);
-
-          var address = '';
-          if (place.address_components) {
-            address = [
-              (place.address_components[0] && place.address_components[0].short_name || ''),
-              (place.address_components[1] && place.address_components[1].short_name || ''),
-              (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-          }
-
-          infowindowContent.children['place-icon'].src = place.icon;
-          infowindowContent.children['place-name'].textContent = place.name;
-          infowindowContent.children['place-address'].textContent = address;
-          infowindow.open(map, marker);
-        });
 
   // Create the places service.
   var service = new google.maps.places.PlacesService(map);
