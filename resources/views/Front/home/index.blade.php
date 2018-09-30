999215de211783ae6c36a7409dfb75e2e02cf700
top-dev <dushan887@gmail.com>
2018-09-30 18:12:27 +0200

    test

--- resources/views/Front/home/index.blade.php
+++ resources/views/Front/home/index.blade.php
@@ -105,9 +105,11 @@ function getMyLocation () {
              if (status == google.maps.GeocoderStatus.OK) {
                $('#address-input').val(results[0].formatted_address);
                var address = results[0].address_components;
-				var zipcode = address[address.length - 1].long_name;
+			   var zipcode = address[address.length - 1].long_name;
+			   var zipcode2 = address[address.length - 2].long_name;
 
 				console.log(zipcode)
+				console.log(zipcode2)
              }
           }); //geocoder.geocode()
         }
