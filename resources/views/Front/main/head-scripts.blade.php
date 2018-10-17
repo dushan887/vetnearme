9934da74f91f6de6599c523799154c96854e86cd
Sasha Miljkovic <imalimalomrk@gmail.com>
2018-10-17 17:27:48 +0200

    adding ga

--- resources/views/Front/main/head-scripts.blade.php
+++ resources/views/Front/main/head-scripts.blade.php
@@ -5,6 +5,16 @@
 
 <link rel="shortcut icon" type="image/png" href="{{ URL::asset('/img/fav.png') }}">
 
+<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-124060217-1', 'auto');
+		ga('send', 'pageview');
+  </script>
+
 <!-- BOOSTRAP 4 -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
