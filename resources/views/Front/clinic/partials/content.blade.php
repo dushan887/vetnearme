0156ad7178ed6cc18ee2584f8ba5fc46ce897a7a
Sasha Miljkovic <imalimalomrk@gmail.com>
2018-10-17 18:43:47 +0200

    still testing ga

--- resources/views/Front/clinic/partials/content.blade.php
+++ resources/views/Front/clinic/partials/content.blade.php
@@ -92,7 +92,7 @@
 							<li id="web-url-check">
 								<a href="{{ $clinic->url }}" target="_blank"
 									rel="nofollow noopener noreferrer"
-									onclick="return trackOutboundLink({{ $clinic->url }}, true)">
+									onclick="return trackOutboundLink('{{ $clinic->url }}', true)">
 									<i class="fas fa-globe"></i> <strong>Website:</strong><br />
 								<span style="margin-top: 0"">{{ $clinic->url }}</span></a></li>
 							<li>
